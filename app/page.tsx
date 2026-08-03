04dbfd2c34ab844fc6fa08b7e533bb700168e876
Deng <d-d@berkeley.edu>
2026-08-02 17:35:12 -0700

    playing around with Ai size

--- app/page.tsx
+++ app/page.tsx
@@ -60,7 +60,7 @@ export default function Home() {
                 // Short hold after focus before revealing main site
                 setTimeout(() => setIsIntroComplete(true), 1200);
               }}
-              className="relative w-64 h-80 md:w-80 md:h-[26rem] rounded-3xl overflow-hidden shadow-2xl border border-black/10"
+              className="relative w-72 md:w-96 rounded-3xl overflow-hidden shadow-2xl border border-black/10"
             >
               <Image
                 src="/Hoshino_Ai.png"
